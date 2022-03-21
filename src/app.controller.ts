b76c6a7928913e624d31f469bf8fc592ecd672cc
take-tech1001 <tk.tech1001@gmail.com>
2022-03-22 04:30:20 +0900

    ♻️ Refactor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import type { OauthV2AccessResponse } from '@slack/web-api';
+import { config } from 'firebase-functions';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getToken(
+    @Req() request: Request,
+  ): Promise<OauthV2AccessResponse | string> {
+    const code = request.query.code as string | undefined;
+    const error = request.query.error as string | undefined;
+    const firebaseConfig = config();
+    const env = firebaseConfig['send-daily-report-e1ce9'];
+    const clientId = env.client_id;
+    const clientSecret = env.client_secret;
+
+    return this.appService.getToken(code, error, clientId, clientSecret);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { WebClient } from '@slack/web-api';
+import type { OauthV2AccessResponse } from '@slack/web-api';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getToken(
+    code: string | undefined,
+    error: string | undefined,
+    clientId: string,
+    clientSecret: string,
+  ): Promise<OauthV2AccessResponse | string> {
+    if (code) {
+      const slack = new WebClient();
+      const result = await slack.oauth.v2.access({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code,
+      });
+      return `access_token: ${result.authed_user.access_token}`;
+    } else if (error) {
+      return '認証がキャンセルされました。';
+    }
   }
 }
